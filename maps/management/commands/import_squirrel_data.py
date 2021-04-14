5288b8e1df417b63c4a2a1bc2c94aefdbcde97b7
jc5506 <peter@nas.local>
2021-04-14 18:40:53 +0800

    fix some bug in model maps.Sight and add import_squirrel_data command to django-admin commend

---
+++ maps/management/commands/import_squirrel_data.py
@@ -0,0 +1,101 @@
+from django.core.management.base import BaseCommand, CommandError
+from maps.models import Sight
+from django.conf import settings
+from datetime import datetime
+from pathlib import Path
+import csv
+import sys
+import os
+
+date_format = '%m%d%Y'
+
+
+def parse_date(v: str):
+    try:
+        return datetime.strptime(v, date_format)
+    except ValueError:
+        pass
+
+
+def parse_boolean(v: str):
+    if v == 'false':
+        return False
+    elif v == 'true':
+        return True
+    return
+
+
+class Command(BaseCommand):
+    help = 'Import squirrels\' data from csv file to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'input_csv_file', type=str,
+            help='Please pass in a csv file with squirrels data you want to import', )
+
+    def handle(self, *args, **options):
+        input_csv_file = Path(options['input_csv_file']).resolve().absolute()
+        if not input_csv_file.exists():
+            self.stderr.write(
+                f'Error occurred while trying to import data. '
+                f'"{input_csv_file}" can not be found.'
+                f' Please pass in a csv file with squirrels data.')
+            sys.exit(1)
+        with open(input_csv_file, 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f, fieldnames=["X", "Y", "Unique Squirrel ID", "Hectare", "Shift", "Date", "Hectare Squirrel Number", "Age", "Primary Fur Color", "Highlight Fur Color", "Combination of Primary and Highlight Color", "Color notes", "Location", "Above Ground Sighter Measurement", "Specific Location", "Running", "Chasing", "Climbing", "Eating", "Foraging", "Other Activities", "Kuks", "Quaas", "Moans", "Tail flags", "Tail twitches", "Approaches", "Indifferent", "Runs from", "Other Interactions", "Lat/Long"],)
+            t = -1
+            for item in reader:
+                if t == -1:
+                    t += 1
+                    continue
+
+                longitude = item['X']
+                latitude = item['Y']
+
+                unique_squirrel_id = item['Unique Squirrel ID']
+                hectare = item['Hectare']
+                shift = item['Shift'],
+                date = parse_date(item['Date'])
+
+                hectare_squirrel_number = item['Hectare Squirrel Number']
+                age = item['Age']
+                primary_fur_color = item['Primary Fur Color']
+                highlight_fur_color = item['Highlight Fur Color']
+                combination_of_primary_and_highlight_color = item['Combination of Primary and Highlight Color']
+                color_notes = item['Color notes']
+                location = item['Location']
+                above_ground_sighter_measurement = item['Above Ground Sighter Measurement']
+                Specific_Location = item['Specific Location']
+                running = parse_boolean(item['Running'])
+                chasing = parse_boolean(item['Chasing'])
+                climbing = parse_boolean(item['Climbing'])
+                eating = parse_boolean(item['Eating'])
+                foraging = parse_boolean(item['Foraging'])
+                other_activities = item['Other Activities']
+                kuks = parse_boolean(item['Kuks'])
+                quaas = parse_boolean(item['Quaas'])
+                moans = parse_boolean(item['Moans'])
+                tail_flags = parse_boolean(item['Tail flags'])
+                tail_twitches = parse_boolean(item['Tail twitches'])
+                approaches = parse_boolean(item['Approaches'])
+                indifferent = parse_boolean(item['Indifferent'])
+                runs_from = parse_boolean(item['Runs from'])
+                other_interactions = item['Other Interactions']
+                Sight.objects.create(
+                    longitude=longitude, latitude=latitude,
+                    unique_squirrel_id=unique_squirrel_id,
+                    hectare=hectare, shift=shift, date=date,
+                    hectare_squirrel_number=hectare_squirrel_number, age=age, primary_fur_color=primary_fur_color,
+                    highlight_fur_color=highlight_fur_color,
+                    combination_of_primary_and_highlight_color=combination_of_primary_and_highlight_color,
+                    color_notes=color_notes, location=location,
+                    above_ground_sighter_measurement=above_ground_sighter_measurement,
+                    Specific_Location=Specific_Location, running=running, chasing=chasing, climbing=climbing,
+                    eating=eating, foraging=foraging, other_activities=other_activities, kuks=kuks, quaas=quaas,
+                    moans=moans, tail_flags=tail_flags, tail_twitches=tail_twitches, approaches=approaches,
+                    indifferent=indifferent, runs_from=runs_from, other_interactions=other_interactions,
+                    )
+                t += 1
+                self.stdout.write(f'{longitude}/{latitude}/{unique_squirrel_id} imported', ending='\n')
+            self.stdout.write(f'{t} rows imported in total')
+
--- maps/models.py
+++ maps/models.py
@@ -19,7 +19,7 @@ class Sight(models.Model):
         blank=False)
     unique_squirrel_id = models.CharField(verbose_name='Unique Squirrel ID', max_length=16, null=False, blank=False)
     hectare = models.CharField(verbose_name='Hectare', max_length=16, null=True, blank=True)
-    shift = models.CharField(verbose_name='Shift', max_length=8, null=True, blank=True),
+    shift = models.CharField(verbose_name='Shift', max_length=8, null=True, blank=True)
     date = models.DateField(verbose_name='Date', null=True, blank=True)
     hectare_squirrel_number = models.PositiveIntegerField(verbose_name='Hectare Squirrel Number', null=True, blank=True)
     age = models.CharField(verbose_name='Age', max_length=16, null=True, blank=True)
@@ -28,14 +28,14 @@ class Sight(models.Model):
     combination_of_primary_and_highlight_color = models.CharField(verbose_name='Combination of Primary and Highlight Color', max_length=128, null=True, blank=True)
     color_notes = models.CharField(verbose_name='Color notes', max_length=128, null=True, blank=True)
     location = models.CharField(verbose_name='Location', max_length=128, null=True, blank=True)
-    above_ground_sighter_measurement = models.PositiveIntegerField(verbose_name='Above Ground Sighter Measurement', null=True, blank=True)
+    above_ground_sighter_measurement = models.CharField(verbose_name='Above Ground Sighter Measurement', max_length=64, null=True, blank=True)
     Specific_Location = models.CharField(verbose_name='Specific Location', max_length=128, null=True, blank=True)
     running = models.BooleanField(verbose_name='Running', null=True, blank=True)
     chasing = models.BooleanField(verbose_name='Chasing', null=True, blank=True)
     climbing = models.BooleanField(verbose_name='Climbing', null=True, blank=True)
     eating = models.BooleanField(verbose_name='Eating', null=True, blank=True)
     foraging = models.BooleanField(verbose_name='Foraging', null=True, blank=True)
-    other_activities = models.BooleanField(verbose_name='Other Activities', null=True, blank=True)
+    other_activities = models.CharField(verbose_name='Other Activities', max_length=128, null=True, blank=True)
     kuks = models.BooleanField(verbose_name='Kuks', null=True, blank=True)
     quaas = models.BooleanField(verbose_name='Quaas', null=True, blank=True)
     moans = models.BooleanField(verbose_name='Moans', null=True, blank=True)
@@ -50,3 +50,9 @@ class Sight(models.Model):
         ordering = '-id',
         verbose_name = 'Sight of Squirrel'
         verbose_name_plural = 'Sight of Squirrels'
+
+
+
+
+
+
