0f19787e12a320d379b93303b217b954a93577db
jc5506 <peter@nas.local>
2021-04-14 21:29:56 +0800

    display 100 squirrels in the map page

--- maps/views.py
+++ maps/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Sight
+# Create your views here.
 
 
 def index(request):
@@ -8,4 +10,14 @@ def index(request):
 
 def map(request):
     """"""
-    return render(request, 'maps/maps.html')
+    data = []
+    t = 0
+    for sight in Sight.objects.all():
+        _ = {'latitude': sight.latitude, 'longitude': sight.longitude}
+        if _ in data:
+            continue
+        data.append(_)
+        t += 1
+        if t >= 100:
+            break
+    return render(request, 'maps/maps.html', context=dict(sightings=data))
