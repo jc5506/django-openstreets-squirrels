523d21b8edaa2d7d7197833a0cb79027e9779111
jc5506 <peter@nas.local>
2021-04-14 16:14:03 +0800

    add two views and routes

---
+++ maps/urls.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from django.urls import path
+
+
+from . import views
+
+app_name = 'maps'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('map', views.map, name='map'),
+    ]
--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """home page"""
+    return render(request, 'maps/index.html')
+
+
+def map(request):
+    """"""
+    return render(request, 'maps/maps.html')
